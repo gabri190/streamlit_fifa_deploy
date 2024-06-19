4227bae0258370d53b51f88e9640d2b876d45ca8
gabri190 <gabriel_avioes@hotmail.com>
2024-06-19 13:30:54 -0300

    link_button add

--- 1_🏠_home.py
+++ 1_🏠_home.py
@@ -14,9 +14,7 @@ st.markdown("# FIFA23 OFFICIAL DATASET! ⚽️")
 st.sidebar.markdown("Desenvolvido por [Asimov Academy](https://asimov.academy)")
 
 
-btn = st.button("Acesse os dados no Kaggle")
-if btn:
-    webbrowser.open_new_tab("https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data")
+btn = st.link_button("Acesse os dados no Kaggle","https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data")
 
 st.markdown(
     """
